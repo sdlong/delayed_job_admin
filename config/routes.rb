7f57bc4da9ffd5329cfb74a2b42c5f94e98d632e
sdlong <sdlong.jeng@gmail.com>
2015-04-08 14:59:33 +0800

    for update rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  match 'delayed_job_admin' => 'delayed_job_admin#index', :as => 'delayed_job_admin'
-  match 'delayed_job_admin/restart' => 'delayed_job_admin#restart', :as => 'delayed_job_admin/restart'
-  match 'delayed_job_admin/check_status' => 'delayed_job_admin#check_status', :as => 'delayed_job_admin/check_status'
+  get 'delayed_job_admin' => 'delayed_job_admin#index', :as => 'delayed_job_admin'
+  get 'delayed_job_admin/restart' => 'delayed_job_admin#restart', :as => 'delayed_job_admin/restart'
+  get 'delayed_job_admin/check_status' => 'delayed_job_admin#check_status', :as => 'delayed_job_admin/check_status'
 
-  match 'delayed_job_admin/delete' => 'delayed_job_admin#delete', :as => 'delete_job_delayed_job_admin', :method => :delete
-  match 'delayed_job_admin/retry' => 'delayed_job_admin#retry_job', :as => 'retry_job_delayed_job_admin', :method => :put
-  match 'delayed_job_admin/run_now' => 'delayed_job_admin#run_job_now', :as => 'run_job_now_delayed_job_admin', :method => :put
+  delete 'delayed_job_admin/delete' => 'delayed_job_admin#delete', :as => 'delete_job_delayed_job_admin', :method => :delete
+  get 'delayed_job_admin/retry' => 'delayed_job_admin#retry_job', :as => 'retry_job_delayed_job_admin', :method => :put
+  get 'delayed_job_admin/run_now' => 'delayed_job_admin#run_job_now', :as => 'run_job_now_delayed_job_admin', :method => :put
 
-  match 'delayed_job_admin/process' => 'delayed_job_admin#process_jobs', :as => 'process_jobs_delayed_job_admin', :method => :put 
+  patch 'delayed_job_admin/process' => 'delayed_job_admin#process_jobs', :as => 'process_jobs_delayed_job_admin', :method => :put
 end
